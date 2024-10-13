d4a2abe9c98b61085c867e404932d8e9aaef2692
bpetermann <benjamin.petermann@gmx.at>
2024-10-13 14:35:49 +0200

    test: add heading tests

---
+++ src/test/a11y/heading.test.ts
@@ -0,0 +1,39 @@
+import { h1Checks, headingChecks } from '../../utils/a11y/heading';
+import { messages } from '../../utils/messages';
+
+describe('Accessibility checks for <h*> elements', () => {
+  it('warns when <h2> is present, but no <h1>', () => {
+    const heading = 'h2';
+
+    const warnings = headingChecks([heading]);
+    expect(warnings).includes(messages.heading.skip + heading);
+  });
+
+  it('warns when <h3> is present, but no <h2>', () => {
+    const heading = 'h3';
+
+    const warnings = headingChecks(['h1', heading]);
+    expect(warnings).includes(messages.heading.skip + heading);
+  });
+
+  it('should pass, when <h2> and <h1> are present', () => {
+    const heading = 'h2';
+
+    const warnings = headingChecks(['h1', heading]);
+    expect(warnings.length).toEqual(0);
+  });
+
+  it('warn when two <h1> elements are present', () => {
+    const headings = ['h1', 'h1'];
+
+    const warnings = h1Checks(headings);
+    expect(warnings).include(
+      messages.heading.unique + headings.filter((h) => h === 'h1').length
+    );
+  });
+
+  it('should pass,when every heading element is present one time', () => {
+    const warnings = headingChecks(['h1', 'h2', 'h4', 'h3', 'h6', 'h5']);
+    expect(warnings.length).toEqual(0);
+  });
+});
