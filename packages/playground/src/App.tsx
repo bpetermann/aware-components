86e819c8a76a69f531362f4681e91f683f191874
bpetermann <benjamin.petermann@gmx.at>
2024-10-23 19:47:41 +0200

    docs: update readme.md

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -1,14 +1,13 @@
-import { A, A11yProvider, Button, Div, H1 } from 'aware-components';
-import { useState } from 'react';
+import { A, A11yProvider, Button, Div, H1, H3 } from 'aware-components';
+import React from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <A11yProvider>
+      <Headings />
       <A href='#main'></A>
       <div>
         <A href='https://vitejs.dev' target='_blank'>
@@ -20,14 +19,18 @@ function App() {
       </div>
       <H1>Vite + React</H1>
       <div className='card'>
+<<<<<<< Updated upstream
         <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
+=======
+>>>>>>> Stashed changes
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <Div>
+<<<<<<< Updated upstream
         <Div>
           <div>
             <Div>
@@ -37,6 +40,9 @@ function App() {
             </Div>
           </div>
         </Div>
+=======
+        <MyButton />
+>>>>>>> Stashed changes
       </Div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
@@ -46,3 +52,31 @@ function App() {
 }
 
 export default App;
+
+function MyButton() {
+  const [, setCount] = React.useState(0);
+
+  return (
+    <Div>
+      <Button
+        style={{
+          backgroundColor: '#000',
+          color: '#333',
+          width: '1em',
+        }}
+        onClick={() => setCount((count) => count + 1)}
+      ></Button>
+    </Div>
+  );
+}
+
+function Headings() {
+  return (
+    <>
+      <H1>Guide to cooking</H1>
+      <H1>Basics of cooking</H1>
+      <H3>Choosing the right flour</H3>
+      <H3>Essential cooking tools</H3>
+    </>
+  );
+}
