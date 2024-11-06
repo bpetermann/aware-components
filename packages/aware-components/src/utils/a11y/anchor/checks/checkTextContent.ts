3962d84c1a2feba223522a41c82a16dbcf995ca4
bpetermann <benjamin.petermann@gmx.at>
2024-11-06 21:38:25 +0100

    feat: add check text content

---
+++ packages/aware-components/src/utils/a11y/anchor/checks/checkTextContent.ts
@@ -0,0 +1,15 @@
+import { ARIA_LABEL, ARIA_LABELLEDBY } from '../../../../constants';
+import { containsImageElement } from '../../../../helper/validation';
+import { messages } from '../../../messages';
+import { AnchorProps } from '../types/AnchorProps';
+
+export const checkTextContext = (
+  props: AnchorProps,
+  text: string
+): string | null =>
+  !text &&
+  !containsImageElement(props.children) &&
+  !props[ARIA_LABEL] &&
+  !props[ARIA_LABELLEDBY]
+    ? messages.anchor.text
+    : null;
