0a707e9feb05891079658c9810fe164eb9f3436b
bpetermann <benjamin.petermann@gmx.at>
2024-11-25 21:14:45 +0100

    chore: add playground component

--- packages/playground/src/components/MyList.tsx
+++ packages/playground/src/components/MyList.tsx
@@ -1,17 +1,17 @@
-import { H2, Li, Ul } from 'aware-components';
+import { H2, Li, Ol } from 'aware-components';
 
 export function MyList() {
   return (
     <>
       <H2>MyList</H2>
-      <Ul style={{ backgroundColor: '#666' }}>
+      <Ol style={{ backgroundColor: '#666' }}>
         <Li>Item 1</Li>
         <Li>Item 2</Li>
         <li>
           <span>Item 3</span>
         </li>
-        <p>Item 5</p>
-      </Ul>
+        <p role='listitem'>Item 5</p>
+      </Ol>
     </>
   );
 }
