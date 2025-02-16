c9a151dbe5d76ef4359316ae7a5d73f3fc64ffd2
bpetermann <benjamin.petermann@gmx.at>
2025-02-16 20:29:57 +0100

    test: change rtl button suite

--- packages/aware-components/src/test/components/Button.test.tsx
+++ packages/aware-components/src/test/components/Button.test.tsx
@@ -23,7 +23,44 @@ describe('Button Component', () => {
     render(<Button />);
 
     expect(warnSpy).toHaveBeenCalledWith(messages.button.text);
+  });
 
-    warnSpy.mockRestore();
+  it('should warn if no text content found', () => {
+    const warnSpy = vi.spyOn(console, 'warn').mockImplementation(() => {});
+    render(<Button role='roletype'>click</Button>);
+
+    expect(warnSpy).toHaveBeenCalledWith(messages.button.role);
+  });
+
+  it('should warn when button has an abstract role', () => {
+    const warnSpy = vi.spyOn(console, 'warn').mockImplementation(() => {});
+    render(<Button role='roletype'>click</Button>);
+
+    expect(warnSpy).toHaveBeenCalledWith(messages.button.role);
+  });
+
+  it('should warn when button has switch role but no aria-checked', () => {
+    const warnSpy = vi.spyOn(console, 'warn').mockImplementation(() => {});
+    render(<Button role='switch'>click</Button>);
+
+    expect(warnSpy).toHaveBeenCalledWith(messages.button.switch);
+  });
+
+  it('should pass if button has no text but <img> child', () => {
+    const warnSpy = vi.spyOn(console, 'warn').mockImplementation(() => {});
+    render(
+      <Button>
+        <img src='icon.png' alt='Icon' />
+      </Button>
+    );
+
+    expect(warnSpy.mock.calls.length).toEqual(0);
+  });
+
+  it('should pass if button has no text but aria-label', () => {
+    const warnSpy = vi.spyOn(console, 'warn').mockImplementation(() => {});
+    render(<Button aria-label='Submit' />);
+
+    expect(warnSpy.mock.calls.length).toEqual(0);
   });
 });
