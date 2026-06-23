d2ab8be5ad84ad1887cdafbca41053eb2bf5c6d9
bpetermann <benjamin.petermann@gmx.at>
2026-06-23 21:01:06 +0200

    chore: cleanup heading check prev heading

--- packages/aware-components/src/utils/a11y/heading/checks/checkPrevHeading.ts
+++ packages/aware-components/src/utils/a11y/heading/checks/checkPrevHeading.ts
@@ -1,9 +1,10 @@
 import { messages } from '../../../messages';
 
 export const checkPrevHeading = (headings: string[]): string | null => {
-  const heading = headings.pop();
+  const heading = headings[headings.length - 1];
+  const previous = headings.slice(0, -1);
 
-  return heading && !headings.includes(`h${+heading[1] - 1}`)
+  return heading && !previous.includes(`h${+heading[1] - 1}`)
     ? `[${heading}] ${messages.heading.skip}`
     : null;
 };
